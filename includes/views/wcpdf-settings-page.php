d53788a0dc800a3c7055c3020c303ea055ba126e
Alexandre Faustino <alexmigf@gmail.com>
2021-08-20 11:34:02 +0100

    Preview concept first commit

--- includes/views/wcpdf-settings-page.php
+++ includes/views/wcpdf-settings-page.php
@@ -7,46 +7,65 @@
 <div class="wrap">
 	<div class="icon32" id="icon-options-general"><br /></div>
 	<h2><?php _e( 'WooCommerce PDF Invoices', 'woocommerce-pdf-invoices-packing-slips' ); ?></h2>
-	<h2 class="nav-tab-wrapper">
-	<?php
-	foreach ($settings_tabs as $tab_slug => $tab_title ) {
-		$tab_link = esc_url("?page=wpo_wcpdf_options_page&tab={$tab_slug}");
-		printf('<a href="%1$s" class="nav-tab nav-tab-%2$s %3$s">%4$s</a>', $tab_link, $tab_slug, (($active_tab == $tab_slug) ? 'nav-tab-active' : ''), $tab_title);
-	}
-	?>
-	</h2>
-
-	<?php
-	do_action( 'wpo_wcpdf_before_settings_page', $active_tab, $active_section );
-
-	// save or check option to hide extensions ad
-	if ( isset( $_GET['wpo_wcpdf_hide_extensions_ad'] ) ) {
-		update_option( 'wpo_wcpdf_hide_extensions_ad', true );
-		$hide_ad = true;
-	} else {
-		$hide_ad = get_option( 'wpo_wcpdf_hide_extensions_ad' );
-	}
 	
-	if ( !$hide_ad && !( class_exists('WooCommerce_PDF_IPS_Pro') && class_exists('WooCommerce_PDF_IPS_Templates') && class_exists('WooCommerce_Ext_PrintOrders') ) ) {
-		include('wcpdf-extensions.php');
-	}
-
-	?>
-	<form method="post" action="options.php" id="wpo-wcpdf-settings" class="<?php echo "{$active_tab} {$active_section}"; ?>">
+	<!-- Settings side -->
+	<div class="settings" style="width:60%; display:inline; float:left; margin-right:0; margin-left:0;">
+		<h2 class="nav-tab-wrapper">
 		<?php
-			do_action( 'wpo_wcpdf_before_settings', $active_tab, $active_section );
-			if ( has_action( 'wpo_wcpdf_settings_output_'.$active_tab ) ) {
-				do_action( 'wpo_wcpdf_settings_output_'.$active_tab, $active_section );
-			} else {
-				// legacy settings
-				settings_fields( "wpo_wcpdf_{$active_tab}_settings" );
-				do_settings_sections( "wpo_wcpdf_{$active_tab}_settings" );
-
-				submit_button();
-			}
-			do_action( 'wpo_wcpdf_after_settings', $active_tab, $active_section );
+		foreach ($settings_tabs as $tab_slug => $tab_title ) {
+			$tab_link = esc_url("?page=wpo_wcpdf_options_page&tab={$tab_slug}");
+			printf('<a href="%1$s" class="nav-tab nav-tab-%2$s %3$s">%4$s</a>', $tab_link, $tab_slug, (($active_tab == $tab_slug) ? 'nav-tab-active' : ''), $tab_title);
+		}
 		?>
+		</h2>
 
-	</form>
-	<?php do_action( 'wpo_wcpdf_after_settings_page', $active_tab, $active_section ); ?>
+		<?php
+		do_action( 'wpo_wcpdf_before_settings_page', $active_tab, $active_section );
+
+		// save or check option to hide extensions ad
+		if ( isset( $_GET['wpo_wcpdf_hide_extensions_ad'] ) ) {
+			update_option( 'wpo_wcpdf_hide_extensions_ad', true );
+			$hide_ad = true;
+		} else {
+			$hide_ad = get_option( 'wpo_wcpdf_hide_extensions_ad' );
+		}
+		
+		if ( !$hide_ad && !( class_exists('WooCommerce_PDF_IPS_Pro') && class_exists('WooCommerce_PDF_IPS_Templates') && class_exists('WooCommerce_Ext_PrintOrders') ) ) {
+			include('wcpdf-extensions.php');
+		}
+
+		?>
+		<form method="post" action="options.php" id="wpo-wcpdf-settings" class="<?php echo "{$active_tab} {$active_section}"; ?>">
+			<?php
+				do_action( 'wpo_wcpdf_before_settings', $active_tab, $active_section );
+				if ( has_action( 'wpo_wcpdf_settings_output_'.$active_tab ) ) {
+					do_action( 'wpo_wcpdf_settings_output_'.$active_tab, $active_section );
+				} else {
+					// legacy settings
+					settings_fields( "wpo_wcpdf_{$active_tab}_settings" );
+					do_settings_sections( "wpo_wcpdf_{$active_tab}_settings" );
+
+					submit_button();
+				}
+				do_action( 'wpo_wcpdf_after_settings', $active_tab, $active_section );
+			?>
+
+		</form>
+		<?php do_action( 'wpo_wcpdf_after_settings_page', $active_tab, $active_section ); ?>
+	</div>
+
+	
+	<!-- Preview side -->
+	<div class="preview" style="width:40%; display:inline-block; height:auto;">
+		<h2 class="nav-tab-wrapper">
+			<a href="#" class="nav-tab nav-tab-preview nav-tab-active">Preview</a>
+		</h2>
+		<div class="preview-wrapper" style="background-color:white; border-left: 1px solid #c3c4c7; border-bottom: 1px solid #c3c4c7; border-right: 1px solid #c3c4c7;">
+			<?php
+				$plugin_folder = trailingslashit( basename(__DIR__) );
+				$preview       = true;
+				include_once( WPO_WCPDF()->plugin_path().'/templates/Simple/invoice.php' );
+			?>
+		</div>
+	</div>
 </div>
